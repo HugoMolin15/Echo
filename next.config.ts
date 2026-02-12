a65fb4e46525098e75083e1cdd4069123d708c12
Echo User <echo@example.com>
2026-02-12 22:10:51 +0100

    Fix: Add Google image domain to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'poseybmunnixsiumwbpu.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
 };
